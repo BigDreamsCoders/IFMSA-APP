9edd3a9d2d03ccf7a11344b10777e7c058a6d789
Pedro Gómez <00057616@uca.edu.sv>
2018-07-15 23:32:56 -0600

    se movieron las actividades a su respectiva carpeta

--- app/src/main/java/com/pedrodeveloper14/ifmsaelsalvador/activities/MainActivity.java
+++ app/src/main/java/com/pedrodeveloper14/ifmsaelsalvador/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pedrodeveloper14.ifmsaelsalvador;
+package com.pedrodeveloper14.ifmsaelsalvador.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pedrodeveloper14.ifmsaelsalvador.R;
 
 public class MainActivity extends AppCompatActivity {
 
